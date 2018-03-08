a872e9f8cc8b229aa5855724fd76fedb8a783c91
Ryan Doner <ridemralphio@yahoo.com>
2018-03-07 22:51:42 -0700

    Removing comments, working on genre routes

--- app/routes/movie_routes.js
+++ app/routes/movie_routes.js
@@ -3,8 +3,28 @@ var ObjectID = require('mongodb').ObjectID;
 var result = [];
 
 module.exports = function(app, db) {
-  
-  //find movie by ID
+ 
+//CRUD Routes:
+
+  //CREATE route
+  app.post('/movies', (req, res) => {
+    //fix this const
+    const movie = { title: req.body.title, id: req.body.id, year: req.body.year, genres: req.body.genres};
+        //returns  "genres": "\"porn\", \"comedy\", \"horror\", \"drama\""
+    //const movie = { text: req.body.body, title: req.body.title };
+    db.collection('movies').insert(movie, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+      //ops Contains the documents inserted with added _id fields
+        //res.send(result.ops[0]);
+        res.send(result.ops[0]);
+        //res.send("posted");
+      }
+    });
+  });
+
+  //READ route
   app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
@@ -17,16 +37,43 @@ module.exports = function(app, db) {
     });
   });
 
-   //find all movies, ordered by year
+  //UPDATE route
+  app.put('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    //fix this line
+    const movie = { title: req.body.title, id: req.body.id, year: req.body.year, genres: req.body.genres};
+    db.collection('movies').update(details, movie, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(movie);
+      } 
+    });
+  });
+
+  //DESTORY route
+  app.delete('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('movies').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Movie ' + id + ' deleted!');
+      } 
+    });
+  });
+
+
+  //Advanced routes: 
+
+  //find all movies, ordered by year
   app.get('/movies', (req,res) => {
-  	//db.collection('movies').find({}).sort({"year": 1}).toArray((err, allMovies) => {
     db.collection('movies').find({}, {"title": 1, _id:0}).sort({"year": 1}).toArray((err, allMovies) => {
-    //    // res.json({ message: 'hooray! welcome to our api!' });   
         if(err) {
           console.log(err);
         } else {
-          //res.send(JSON.stringify(allMovies));
-          //console.log(JSON.stringify(allMovies));
           res.send(allMovies);
         } 
     });          
@@ -55,10 +102,10 @@ module.exports = function(app, db) {
   app.get('/genres', (req,res) => {
     
     //testing this route:
-    db.collection('movies').aggregate([{$group : {_id : "$genres", num_movies : {$sum : 1}}}, {$sort: {"num_movies": -1}}], 
+    //db.collection('movies').aggregate([{$group : {_id : "$genres", num_movies : {$sum : 1}}}, {$sort: {"num_movies": -1}}], 
    
     //working, but unordered route:
-    //db.collection('movies').distinct("genres", (err, result) => {
+    db.collection('movies').distinct("genres",
     
     //fluff:
     //db.collection('movies').find({}, {"title": 1, _id:0}).sort({"year": 1}).toArray((err,result) => {
@@ -76,51 +123,34 @@ module.exports = function(app, db) {
     });
   });
 
-  //list genres by those containing the most movies
 
-
-  app.post('/movies', (req, res) => {
-    //fix this const
-    const movie = { title: req.body.title, id: req.body.id, year: req.body.year, genres: req.body.genres};
-        //returns  "genres": "\"porn\", \"comedy\", \"horror\", \"drama\""
-    //const movie = { text: req.body.body, title: req.body.title };
-    db.collection('movies').insert(movie, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
-      } else {
-      //ops Contains the documents inserted with added _id fields
-        //res.send(result.ops[0]);
-        res.send(result.ops[0]);
-        //res.send("posted");
-      }
+//An endpoint for a particular genre that includes a listing of the genre’s movie(s).
+  app.get('/genremovies', (req,res) => {
+    //const genre = req.params.genre;
+    db.collection('movies').aggregate([
+          {$group: {_id: "$genres" /*, genres: { $push: "$genres" */}},
+          {$sort: {genre:-1}}
+          ]
+      (err, result) => {
+        if(err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        } 
     });
   });
-
-  app.delete('/movies/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    db.collection('movies').remove(details, (err, item) => {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send('Movie ' + id + ' deleted!');
-      } 
-    });
-  });
-
-  app.put('/movies/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    //fix this line
-    const movie = { title: req.body.title, id: req.body.id, year: req.body.year, genres: req.body.genres};
-    db.collection('movies').update(details, movie, (err, result) => {
-      if (err) {
-          res.send({'error':'An error has occurred'});
-      } else {
-          res.send(movie);
-      } 
-    });
-  });
+  //READ route
+  // app.get('/movies/:id', (req, res) => {
+  //   const id = req.params.id;
+  //   const details = { '_id': new ObjectID(id) };
+  //   db.collection('movies').findOne(details, (err, item) => {
+  //     if (err) {
+  //       res.send({'error':'An error has occurred'});
+  //     } else {
+  //       res.send(item);
+  //     } 
+  //   });
+  // });
 
 };
 
